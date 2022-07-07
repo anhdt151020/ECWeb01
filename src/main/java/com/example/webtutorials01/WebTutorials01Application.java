76a0597d1abe2259f0bab947abfadbb015d596ba
Đinh Tuấn Anh <dinhtuananh@inhs-MacBook-Pro.local>
2022-07-07 20:22:54 +0700

    first commit

---
+++ src/main/java/com/example/webtutorials01/WebTutorials01Application.java
@@ -0,0 +1,13 @@
+package com.example.webtutorials01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTutorials01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebTutorials01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webtutorials01/WebTutorials01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webtutorials01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTutorials01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
